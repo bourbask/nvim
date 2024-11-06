450045c28bf08fdc9ae8e97a2fa6e28fac6032ae
Kevin Bourbasquet <bourbasquet.k@etik.com>
2024-11-06 13:13:23 +0100

    impr: fully sync with the destination

--- install.sh
+++ install.sh
@@ -6,8 +6,10 @@ SOURCE_DIR="$(pwd)"
 # Determine destination based on the OS
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     DEST_DIR="$HOME/.config/nvim"
+    SYNC_CMD="rsync -av --delete \"$SOURCE_DIR/\" \"$DEST_DIR/\""
 elif [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "win32" ]]; then
     DEST_DIR="/c/Users/KREDG/AppData/Local/nvim"
+    SYNC_CMD="robocopy \"$SOURCE_DIR\" \"$DEST_DIR\" /MIR /NFL /NDL /NJH /NJS /nc /ns /np"
 else
     echo "Unsupported OS: $OSTYPE"
     exit 1
@@ -16,9 +18,9 @@ fi
 # Create the destination directory if it doesn't exist
 mkdir -p "$DEST_DIR"
 
-# Copy the content of nvim folder to the destination
-echo "Copying nvim configuration to $DEST_DIR..."
-cp -r "$SOURCE_DIR/"* "$DEST_DIR"
+# Run the sync command
+echo "Synchronizing nvim configuration to $DEST_DIR..."
+eval "$SYNC_CMD"
 
 # Confirm completion
-echo "nvim configuration successfully copied to $DEST_DIR"
+echo "nvim configuration successfully synchronized to $DEST_DIR"
