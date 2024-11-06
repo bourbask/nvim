dcc46199da8020fa9d1427da43c5da4629630187
Kevin Bourbasquet <bourbasquet.k@etik.com>
2024-11-06 12:16:27 +0100

    fix: wrong init update

--- init.lua
+++ init.lua
@@ -1,76 +1,14 @@
--- lua/plugins/init.lua
+-- init.lua
 
--- Ensure Lazy.nvim is installed
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", lazypath
-    })
-end
+-- Load settings
+require("settings")
 
-vim.opt.rtp:prepend(lazypath)
+-- Load plugins
+require("plugins")
 
-require("lazy").setup({
-    { "nvim-lualine/lualine.nvim", config = function() require("plugins.lualine") end },
-    { 
-        "folke/tokyonight.nvim", 
-        config = function()
-            vim.cmd("colorscheme tokyonight-night")
-        end
-    },
-    { "williamboman/mason.nvim", config = function() require("plugins.mason") end },
-    "tpope/vim-surround",
+-- Load key mappings
+require("keymaps")
 
-    -- LSP and tools
-    "neovim/nvim-lspconfig",            -- LSP configurations
-    "williamboman/mason.nvim",          -- LSP manager
-    "williamboman/mason-lspconfig.nvim", -- Helper for auto setting up LSP servers
+-- Load autocommands
+require("autocommands")
 
-    -- Alpha welcome screen
-    { "goolord/alpha-nvim", config = function() require("plugins.alpha") end },
-
-    -- Telescope for fuzzy search and grep
-    { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" }, config = function() require("plugins.telescope") end },
-
-    -- Markdown preview
-    { "iamcco/markdown-preview.nvim", build = "cd app && npm install" },
-
-    -- Debugging (DAP)
-    "mfussenegger/nvim-dap",
-    {
-        "rcarriga/nvim-dap-ui",
-        config = function()
-            local dap, dapui = require("dap"), require("dapui")
-            dapui.setup()
-            dap.listeners.after.event_initialized["dapui_config"] = function()
-                dapui.open()
-            end
-            dap.listeners.before.event_terminated["dapui_config"] = function()
-                dapui.close()
-            end
-            dap.listeners.before.event_exited["dapui_config"] = function()
-                dapui.close()
-            end
-        end,
-    },
-    "mfussenegger/nvim-dap-python",   -- DAP for Python
-
-    -- nvim-nio dependency
-    "nvim-neotest/nvim-nio",
-
-    -- Linter and Formatter
-    "jose-elias-alvarez/null-ls.nvim"
-})
-
--- Load LSP configurations
-require("plugins.lsp")
-
--- Load DAP configurations
-require("plugins.dap")
-
--- Load null-ls configuration
-require("plugins.null-ls")
